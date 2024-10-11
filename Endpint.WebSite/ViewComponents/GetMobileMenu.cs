b75ce84079f5a662e0237e501efc685d67ade01d
semojan <97410424+semojan@users.noreply.github.com>
2024-10-11 11:49:11 +0330

    dynamic mobile menu

---
+++ Endpint.WebSite/ViewComponents/GetMobileMenu.cs
@@ -0,0 +1,21 @@
+﻿using _04_06_01_ecommerce.Application.Services.Common.Queeries.GetMenuItem;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endpint.WebSite.ViewComponents
+{
+    public class GetMobileMenu : ViewComponent
+    {
+        private readonly IGetMenuItemService _getMenuItem;
+        public GetMobileMenu(IGetMenuItemService getMenuItem)
+        {
+            _getMenuItem = getMenuItem;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = _getMenuItem.Execute().Data;
+            return View(viewName: "GetMobileMenu", menuItems);
+        }
+    }
+}
